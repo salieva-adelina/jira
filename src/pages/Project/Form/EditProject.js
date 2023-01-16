fde19e062e9d8c38f3af094f8c85bba91dd6b0ad
kortess1999 <58464041+kortess1999@users.noreply.github.com>
2023-01-16 17:02:49 +0300

    project edit

--- src/pages/Project/Form/EditProject.js
+++ src/pages/Project/Form/EditProject.js
@@ -7,25 +7,58 @@ import { CREATE_PROJECT_SAGA, DUPPLICATE_PROJECT_NAME } from '../../../redux/con
 import Swal from 'sweetalert2'
 import axios from "axios";
 import { SERVER_API_URL, commonHeaders } from "../../../util/config/constants";
+import {getCookie} from "../../../util/libs/cookie";
 
 function EditProject(props) {
     const {
         // errors,
         handleChange,
-        handleSubmit,
         setFieldValue,
         // values,
         // touched,
         // handleBlur,
     } = props;
+    const projectId = Number(props.match.params.id);
+
+    const nameRef = useRef();
+    const [name, setName] = useState('');
+
+    const managerRef = useRef();
+    const [manager, setManager] = useState('');
+
+    const  [isArchive, setIsArchive] = useState(false);
+
+    const descriptionRef = useRef();
+    const [description, setDescription] = useState('');
+
+
+    const [createdDate, setCreatedDate] = useState('')
 
     const dispatch = useDispatch();
-
     const [users, setUsers] = useState([]);
+
+    const getProject = () => {
+        if (!projectId) {
+            return;
+        }
+        axios.post(`${SERVER_API_URL}/project`,
+            JSON.stringify({
+                projectId: projectId,
+            }),
+            commonHeaders
+        ).then((res) => {
+            const project = res.data ?? {};
+            setName(project.name)
+            setManager(project.manager)
+            setIsArchive(project.isArchive)
+            setDescription(project.description)
+                    }).catch((e) => console.log(e));
+        //setTasks(tasksExample);
+    }
 
     const getAllUsers = () => {
         axios.post(
-            `${SERVER_API_URL}/users`)
+            `${SERVER_API_URL}/users`,"",commonHeaders)
             .then((res) => {
                 const users = res.data?.users ?? [];
                 setUsers(users);
@@ -36,6 +69,7 @@ function EditProject(props) {
     //Updating Users onLoad:
     useEffect(() => {
         getAllUsers();
+        getProject();
     }, []);
 
     useEffect(() => {
@@ -65,6 +99,22 @@ function EditProject(props) {
 
     }, [props.dupplicateProjectName]);
 
+    const handleSubmit = () => {
+        axios.post(
+            `${SERVER_API_URL}/projects/change`,
+            JSON.stringify({
+                id: projectId,
+                name: name,
+                manager: manager,
+                description: description,
+                isArchive: isArchive
+            }),
+            commonHeaders
+        ).then((res) =>{
+                props.history.goBack();
+        }).catch((e) => console.log(e));
+    };
+
     const handleEditorChange = (content, editor) => {
         setFieldValue('description', content);
     };
@@ -74,17 +124,25 @@ function EditProject(props) {
             <div style={{ width: '60%' }} className="mt-4">
                 <div className="mb-4">
                     <label className="form-label">Название</label>
-                    <input className="form-control" name="name" placeholder="Название проекта" required="required" onChange={handleChange} />
+                    <input className="form-control"
+                           name="name"
+                           placeholder="Название проекта"
+                           required="required"
+                           ref={nameRef}
+                           value={name}
+                           onChange={()=>{
+                               setName(nameRef.current.value)
+                           }}/>
                 </div>
                 <div className="form-check">
                     <input
                         className="form-check-input"
                         type="checkbox"
                         id="isArchive"
-                        name="isArchive"
                         aria-expanded="false"
                         aria-controls="isArchive"
-                        onChange={handleChange}
+                        checked={isArchive}
+                        onChange={()=>setIsArchive(!isArchive)}
                     />
                     <label className="form-check-label" htmlFor="isArchive">Архивный проект</label>
                 </div>
@@ -97,7 +155,11 @@ function EditProject(props) {
                         className="form-select"
                         aria-label="Adding user to project"
                         name="manager"
-                        onChange={handleChange}
+                        ref={managerRef}
+                        value={manager}
+                        onChange={()=>{
+                            setManager(managerRef.current.value)
+                        }}
                     >
                         {users.map((user) =>
                             <option value={user} key={user}>{user}</option>
@@ -108,6 +170,11 @@ function EditProject(props) {
                     <label className="form-label">Описание</label>
                     <Editor
                         name="description"
+                        ref={descriptionRef}
+                        value={description}
+                        onChange={()=>{
+                            setDescription(descriptionRef.current.value)
+                        }}
                         initialValue=""
                         init={{
                             height: 300,
@@ -123,6 +190,7 @@ function EditProject(props) {
                                 'removeformat | help',
                             content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
                         }}
+
                         onEditorChange={handleEditorChange}
                     />
                 </div>
@@ -154,17 +222,7 @@ const CreateProjectWithFormik = withFormik({
     // validationSchema: Yup.object().shape({
     // }),
 
-    handleSubmit: (values, { setSubmitting, props }) => {
-        setSubmitting(false);
-        axios.post(
-            `${SERVER_API_URL}/projects/change`,
-            JSON.stringify({
-                ...values,
-            }),
-            commonHeaders
-        ).then((res) => props.history.goBack())
-            .catch((e) => console.log(e));
-    },
+
 
     displayName: 'УПиЗ',
 })(EditProject);
